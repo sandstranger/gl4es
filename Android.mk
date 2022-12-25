a70f87309d9a24ab49e7e1992add60492ec2cc5c
Evgeniy Mineev <e.mineev@crazypanda.ru>
2022-12-25 21:20:51 +0400

    axdwdwdwadwadwdw

--- Android.mk
+++ Android.mk
@@ -89,6 +89,6 @@ LOCAL_CFLAGS += -DNO_GBM
 LOCAL_LDLIBS := -ldl -llog
 #building as a static lib
 
-LOCAL_CFLAGS += -DSTATICLIB
-include $(BUILD_STATIC_LIBRARY)
+#LOCAL_CFLAGS += -DSTATICLIB
+include $(BUILD_SHARED_LIBRARY)
 
