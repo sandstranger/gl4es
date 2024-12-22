2cce01015e3f1c26d44a72cd410562a512aaeb5a
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-12-22 15:40:41 +0400

    build shared lib

--- Android.mk
+++ Android.mk
@@ -89,9 +89,11 @@ LOCAL_SRC_FILES := \
 	src/glx/gbm.c \
 	src/glx/streaming.c \
 
-LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -std=gnu99 -funwind-tables -flto=thin -O3 -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
+LOCAL_CFLAGS += -DUSE_ANDROID_LOG=1
+LOCAL_LDFLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls -fuse-ld=lld
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
 LOCAL_CFLAGS += -DDEFAULT_ES=2
 //TODO: maybe temporary?
@@ -100,5 +102,5 @@ LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else
 LOCAL_LDLIBS := -llog
 #building as a static lib
 
-LOCAL_CFLAGS += -DSTATICLIB
-include $(BUILD_STATIC_LIBRARY)
+#LOCAL_CFLAGS += -DSTATICLIB
+include $(BUILD_SHARED_LIBRARY)
