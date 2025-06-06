bbf474bb2e44ca4c406eacf8cc4b03fcdc5dca80
Evgeniy Mineev <e.mineev@crazypanda.ru>
2025-06-06 11:23:33 +0400

    Revert "use -O2 instead -O3"
    This reverts commit 35d44f6c801f3142b99895f6f93b46d8ec69b3db.

--- Android.mk
+++ Android.mk
@@ -89,7 +89,7 @@ LOCAL_SRC_FILES := \
 	src/glx/gbm.c \
 	src/glx/streaming.c \
 
-LOCAL_CFLAGS += -std=gnu99 -funwind-tables -flto=thin -O2 -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -std=gnu99 -funwind-tables -flto=thin -O3 -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 LOCAL_CFLAGS += -DUSE_ANDROID_LOG=1
