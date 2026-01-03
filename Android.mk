5095836d193f211649eaa07901bde03236653df8
Evgeniy Mineev <e.mineev@crazypandagames.com>
2026-01-03 22:43:55 +0400

    remove -O3

--- Android.mk
+++ Android.mk
@@ -91,7 +91,7 @@ LOCAL_SRC_FILES := \
 	src/glx/gbm.c \
 	src/glx/streaming.c \
 
-LOCAL_CFLAGS += -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include include/android_debug.h
+LOCAL_CFLAGS += -std=gnu99 -funwind-tables -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
